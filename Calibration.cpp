9a87ab1648b6ed76e43630e599ef3a46cf576fb7
Ma <839248455@qq.com>
2018-06-08 21:05:30 +0800

    second commit

---
+++ Calibration.cpp
@@ -0,0 +1,14 @@
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <fstream>
+
+using namespace cv;  // 标定前注意更改程序 的网格点数 和 网格大小参数
+using namespace std;
+
+void main()
+{
+
+}
